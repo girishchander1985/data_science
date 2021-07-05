250eaf758b1f3b223ece389c14a936aa48a76653
Girish Chander <girishchander1985@gmail.com>
2021-07-05 16:06:34 +0530

    initial efficiency index

---
+++ BIS_efficiency_index.R
@@ -0,0 +1,42 @@
+# install.packages("RPostgreSQL")
+require("RPostgreSQL")
+
+# create a connection
+# save the password that we can "hide" it as best as we can by collapsing it
+pw <- {
+  "loader@123"
+}
+
+# loads the PostgreSQL driver
+drv <- dbDriver("PostgreSQL")
+# creates a connection to the postgres database
+# note that "con" will be used later in each connection to the database
+con <- dbConnect(drv, dbname = "bismanak_020721",
+                 host = "localhost", port = 5444,
+                 user = "enterprisedb", password = pw)
+rm(pw) # removes the password
+
+# check for the cartable
+dbExistsTable(con, "data_manak_eff_index_emp")
+# TRUE
+df_postgres <- dbGetQuery(con, " select * from  dashboard.data_manak_eff_index_emp where batch_id=(select max(batch_id) from dashboard.data_manak_eff_index_emp) ")
+# emp data
+
+df_postgres 
+
+barplot(df_postgres[1,12])
+
+head(df_postgres, n = 10)
+#end
+
+
+
+wim5 <-dbGetQuery(con,"select * from  dashboard.data_manak_eff_index where batch_id=(select max(batch_id) from dashboard.data_manak_eff_index ) order by eff_index  desc" )
+
+head(wim5, n = 10)
+
+barplot (wim5[,10],xlab = "Branch Name",
+         ylab = "Efficiency index", names.arg=wim5$str_branch_name,col='blue') 
+
+
+
